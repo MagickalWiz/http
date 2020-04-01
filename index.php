d87897734c36a1b9a2a380366a1a1f93e8085a23
MagickalWiz <54958629+MagickalWiz@users.noreply.github.com>
2020-04-01 14:12:53 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+  <meta http-equiv="refresh" content="4;url=http://localhost/homepg.html">
+      <title>MagickalWiz - The Wizards</title>
+      <link rel="icon" 
+      type="image/png" 
+      href="code.png">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+<div class="container">
+  <span class="text1"><font color="#008d00">The Wizards</font></span>
+  <img src = "wizlogo.png" >
+  <span class="text1"><font color="#008d00">MagickalWiz</font></span>
+</div>
+  </body>
+</html>
